be37fbbea013c0b1d6098f07b6c156f234215096
sweesweett <sweesweet@naver.com>
2022-06-06 19:21:51 +0900

    시계 css 하기 전

--- clock.js
+++ clock.js
@@ -1,8 +1,21 @@
-const hour = document.querySelector('.hour');
-const minute = document.querySelector('.minute');
-const second = document.querySelector('.second');
+const btns = document.querySelector('.btns');
 
-function setClock() {
+btns.onclick = (e) => {
+  const analogContainer = document.querySelector('.clock__analog__container');
+  const digitalContainer = document.querySelector('.clock__digital__container');
+  if (e.target.className === 'analog') {
+    analogContainer.style.display = 'flex';
+    digitalContainer.style.display = 'none';
+  } else if (e.target.className === 'digital') {
+    analogContainer.style.display = 'none';
+    digitalContainer.style.display = 'flex';
+  }
+};
+
+function setAnalogClock() {
+  const hour = document.querySelector('.hour');
+  const minute = document.querySelector('.minute');
+  const second = document.querySelector('.second');
   let now = new Date();
   let getHours = now.getHours() * 30;
   let getMinutes = now.getMinutes() * 6;
@@ -11,5 +24,38 @@ function setClock() {
   minute.style.transform = `rotateZ(${getMinutes}deg)`;
   hour.style.transform = `rotateZ(${getHours + getMinutes / 12}deg)`;
 }
-setClock();
-setInterval(setClock, 1000);
+setAnalogClock();
+setInterval(setAnalogClock, 1000);
+
+function setDigitalClock() {
+  const hour = document.querySelector('.hours');
+  const minute = document.querySelector('.minutes');
+  const second = document.querySelector('.seconds');
+  const amPm = document.querySelector('.ampm');
+  let now = new Date();
+  let isAmPm = 'AM';
+  let getHours = now.getHours();
+
+  let issmaller10 = function (num) {
+    if (num < 10) {
+      num = '0' + num;
+      return num;
+    }
+    return num;
+  };
+
+  if (getHours > 12) {
+    getHours = issmaller10(24 - getHours);
+    isAmPm = 'PM';
+  }
+
+  let getMinutes = issmaller10(now.getMinutes());
+  let getSeconds = issmaller10(now.getSeconds());
+
+  hour.textContent = getHours;
+  minute.textContent = getMinutes;
+  second.textContent = getSeconds;
+  amPm.textContent = isAmPm;
+}
+setDigitalClock();
+setInterval(setDigitalClock, 1000);
