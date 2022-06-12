a9fc2d279ebc280214eed05e96474c5f16d33bac
sweesweett <sweesweet@naver.com>
2022-06-12 19:37:43 +0900

    css제외,setInterval 오류 수정 제외

--- rocksp.js
+++ rocksp.js
@@ -1,31 +1,111 @@
-let isRotate = false;
+toLocalStorage();
+const icons = document.querySelector('#toShowNotResult');
+const resultMessage = document.querySelector('.result__message');
+const btns = document.querySelector('.btns');
+const restart = document.querySelector('.restart');
+let isRotate = true;
 const test = [
   'fa-solid fa-hand-scissors',
   'fa-solid fa-hand',
   'fa-solid fa-hand-back-fist',
 ];
-const testdom = document.querySelector('.test_1');
-const testdom2 = document.querySelector('.test_2');
-const btn = document.querySelector('.test');
-const btn2 = document.querySelector('.test2');
-// btn.onclick = () => {
+let interval = itsInterval;
 
-//   console.log('test중인데 망한것같아');
-//   testdom.className = test[i];
-//   i++;
-// };
-
-const itsInterval = setInterval(() => {
-  let xx = 0.1;
-  for (let el of test) {
-    setTimeout(() => (testdom.className = el), xx * 1000);
-    xx += 0.1;
+//로컬스토리지 초기 설정
+function toLocalStorage() {
+  const userScore = document.querySelector('.user__score');
+  const computerScore = document.querySelector('.computer__score');
+  let localResult = JSON.parse(localStorage.getItem('result'));
+  if (localResult === null) {
+    userScore.textContent = `you:0`;
+    computerScore.textContent = `computer:0`;
+    localResult.computer = localResult.computer + 1;
+    return localStorage.setItem(
+      'result',
+      JSON.stringify({ computer: 0, user: 0 })
+    );
+  } else {
+    userScore.textContent = `you:${localResult.user}`;
+    computerScore.textContent = `computer:${localResult.computer}`;
   }
-}, 100);
-btn2.onclick = (e) => {
+  return;
+}
+
+//가위바위보 아이콘 돌아가게하기
+function itsInterval() {
+  setInterval(() => {
+    let xx = 0.1;
+    for (let el of test) {
+      setTimeout(() => (icons.className = el), xx * 1000);
+      xx += 0.1;
+    }
+  }, 100);
+}
+
+//가위바위보 버튼 클릭했을 때 이벤트
+btns.addEventListener('click', (e) => {
+  const userResult = document.querySelector('#userResult');
+  const iconsResult = document.querySelector('#realResult');
+
+  if (isRotate === false) {
+    return;
+  }
+  if (test.includes(e.target.className) === false) {
+    return;
+  }
+  isRotate = !isRotate;
   clearInterval(itsInterval);
-  testdom.style.display = 'none';
-  testdom2.className = test[Math.floor(Math.random() * test.length)];
-  testdom2.style.display = 'block';
-};
-itsInterval;
+  interval = null;
+  console.log(interval);
+  icons.style.display = 'none';
+  iconsResult.className = test[Math.floor(Math.random() * test.length)];
+  iconsResult.style.display = 'block';
+  userResult.className = e.target.className;
+
+  if (e.target.className === iconsResult.className) {
+    gameResult('tie');
+  } else if (e.target.className === test[0]) {
+    if (iconsResult.className === test[2]) {
+      gameResult('lose');
+    } else {
+      gameResult('win');
+    }
+  } else if (e.target.className === test[1]) {
+    if (iconsResult.className === test[0]) {
+      gameResult('lose');
+    } else {
+      gameResult('win');
+    }
+  } else if (e.target.className === test[2]) {
+    if (iconsResult.className === test[1]) {
+      gameResult('lose');
+    } else {
+      gameResult('win');
+    }
+  }
+});
+
+//게임 결과값
+function gameResult(result) {
+  let localResult = JSON.parse(localStorage.getItem('result'));
+  const userScore = document.querySelector('.user__score');
+  const computerScore = document.querySelector('.computer__score');
+  console.log(localResult);
+  if (result === 'win') {
+    resultMessage.textContent = 'YOU WIN!';
+    localResult.user = localResult.user + 1;
+    userScore.textContent = `you:${localResult.user}`;
+    console.log(localResult);
+    localStorage.setItem('result', JSON.stringify(localResult));
+  } else if (result === 'tie') {
+    resultMessage.textContent = "IT'S A TIE!";
+  } else {
+    resultMessage.textContent = 'YOU LOSE!';
+    localResult.computer = localResult.computer + 1;
+    computerScore.textContent = `:${localResult.computer}`;
+    localStorage.setItem('result', JSON.stringify(localResult));
+  }
+}
+
+itsInterval();
+restart.addEventListener('click', itsInterval);
