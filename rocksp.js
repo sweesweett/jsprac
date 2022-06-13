95ce80fa196fddb1aa1d8fee6359e255f751732c
sweesweett <sweesweet@naver.com>
2022-06-13 14:16:15 +0900

    기능구현 완료 css제외

--- rocksp.js
+++ rocksp.js
@@ -1,25 +1,32 @@
-toLocalStorage();
 const icons = document.querySelector('#toShowNotResult');
 const resultMessage = document.querySelector('.result__message');
 const btns = document.querySelector('.btns');
 const restart = document.querySelector('.restart');
+const initialization = document.querySelector('.initialization');
+const userScore = document.querySelector('.user__score');
+const computerScore = document.querySelector('.computer__score');
+const iconsResult = document.querySelector('#realResult');
 let isRotate = true;
+let itsInterval = null;
 const test = [
   'fa-solid fa-hand-scissors',
   'fa-solid fa-hand',
   'fa-solid fa-hand-back-fist',
 ];
-let interval = itsInterval;
-
+window.onload = () => {
+  toLocalStorage();
+  startInterval();
+};
 //로컬스토리지 초기 설정
 function toLocalStorage() {
   const userScore = document.querySelector('.user__score');
   const computerScore = document.querySelector('.computer__score');
   let localResult = JSON.parse(localStorage.getItem('result'));
+  resultMessage.textContent = '가위 바위 보!';
   if (localResult === null) {
     userScore.textContent = `you:0`;
     computerScore.textContent = `computer:0`;
-    localResult.computer = localResult.computer + 1;
+
     return localStorage.setItem(
       'result',
       JSON.stringify({ computer: 0, user: 0 })
@@ -31,32 +38,34 @@ function toLocalStorage() {
   return;
 }
 
-//가위바위보 아이콘 돌아가게하기
-function itsInterval() {
-  setInterval(() => {
+// 가위바위보 아이콘 돌아가게하기
+function startInterval() {
+  isRotate = true;
+  iconsResult.style.display = 'none';
+  icons.style.display = 'block';
+  itsInterval = setInterval(() => {
     let xx = 0.1;
-    for (let el of test) {
+    test.forEach((el) => {
       setTimeout(() => (icons.className = el), xx * 1000);
       xx += 0.1;
-    }
+    });
   }, 100);
 }
-
-//가위바위보 버튼 클릭했을 때 이벤트
+//interval 정지
+function stopInterval() {
+  clearInterval(itsInterval);
+  isRotate = false;
+}
+// 가위바위보 버튼 클릭했을 때 이벤트
 btns.addEventListener('click', (e) => {
   const userResult = document.querySelector('#userResult');
-  const iconsResult = document.querySelector('#realResult');
-
   if (isRotate === false) {
     return;
   }
   if (test.includes(e.target.className) === false) {
     return;
   }
-  isRotate = !isRotate;
-  clearInterval(itsInterval);
-  interval = null;
-  console.log(interval);
+  stopInterval();
   icons.style.display = 'none';
   iconsResult.className = test[Math.floor(Math.random() * test.length)];
   iconsResult.style.display = 'block';
@@ -88,8 +97,7 @@ btns.addEventListener('click', (e) => {
 //게임 결과값
 function gameResult(result) {
   let localResult = JSON.parse(localStorage.getItem('result'));
-  const userScore = document.querySelector('.user__score');
-  const computerScore = document.querySelector('.computer__score');
+
   console.log(localResult);
   if (result === 'win') {
     resultMessage.textContent = 'YOU WIN!';
@@ -102,10 +110,84 @@ function gameResult(result) {
   } else {
     resultMessage.textContent = 'YOU LOSE!';
     localResult.computer = localResult.computer + 1;
-    computerScore.textContent = `:${localResult.computer}`;
+    computerScore.textContent = `computer:${localResult.computer}`;
     localStorage.setItem('result', JSON.stringify(localResult));
   }
 }
+initialization.addEventListener('click', () => {
+  localStorage.setItem('result', JSON.stringify({ computer: 0, user: 0 }));
+  userScore.textContent = 'you:0';
+  computerScore.textContent = 'computer:0';
+  //초기화 하고 나면 자동으로 돌 수 있도록 해야겠음
+});
+restart.addEventListener('click', () => {
+  isRotate = true;
+  startInterval();
+});
 
-itsInterval();
-restart.addEventListener('click', itsInterval);
+// function gameClosure() {
+//   function game() {
+//     /* This is just an example, replace this with the body of gameInit() */
+//     console.log('The game is running');
+//   }
+
+//   let itsInterval;
+
+//   return {
+//     start() {
+//       isRotate = !isRotate;
+//       itsInterval = setInterval(() => {
+//         let xx = 0.1;
+//         for (let el of test) {
+//           setTimeout(() => (icons.className = el), xx * 1000);
+//           xx += 0.1;
+//         }
+//       }, 100);
+//     },
+//     stop() {
+//       const userResult = document.querySelector('#userResult');
+//       const iconsResult = document.querySelector('#realResult');
+//       btns.addEventListener('click', (e) => {
+//         if (isRotate === false) {
+//           return;
+//         }
+//         if (test.includes(e.target.className) === false) {
+//           return;
+//         }
+//         isRotate = !isRotate;
+//         clearInterval(itsInterval);
+//         icons.style.display = 'none';
+//         iconsResult.className = test[Math.floor(Math.random() * test.length)];
+//         iconsResult.style.display = 'block';
+//         userResult.className = e.target.className;
+
+//         if (e.target.className === iconsResult.className) {
+//           gameResult('tie');
+//         } else if (e.target.className === test[0]) {
+//           if (iconsResult.className === test[2]) {
+//             gameResult('lose');
+//           } else {
+//             gameResult('win');
+//           }
+//         } else if (e.target.className === test[1]) {
+//           if (iconsResult.className === test[0]) {
+//             gameResult('lose');
+//           } else {
+//             gameResult('win');
+//           }
+//         } else if (e.target.className === test[2]) {
+//           if (iconsResult.className === test[1]) {
+//             gameResult('lose');
+//           } else {
+//             gameResult('win');
+//           }
+//         }
+//       });
+//     },
+//   };
+// }
+
+// let game = gameClosure();
+
+// btns.onclick = game.stop();
+// restart.onclick = game.start();
