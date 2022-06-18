5b1a34d47fc8992d13ad622a067558d98039f699
sweesweett <sweesweet@naver.com>
2022-06-18 20:19:57 +0900

    빵빠레+ , 이모지 전 , css 마무리x

--- rocksp.js
+++ rocksp.js
@@ -44,10 +44,10 @@ function startInterval() {
   iconsResult.style.display = 'none';
   icons.style.display = 'block';
   itsInterval = setInterval(() => {
-    let xx = 0.1;
+    let xx = 0.2;
     test.forEach((el) => {
       setTimeout(() => (icons.className = el), xx * 1000);
-      xx += 0.1;
+      xx += 0.2;
     });
   }, 100);
 }
@@ -102,89 +102,83 @@ function gameResult(result) {
     localResult.user = localResult.user + 1;
     userScore.textContent = `you : ${localResult.user}`;
     localStorage.setItem('result', JSON.stringify(localResult));
+    animateParticles({ total: 150 });
   } else if (result === 'tie') {
     resultMessage.textContent = '비겼어요!';
+
   } else {
     resultMessage.textContent = '졌어요...';
     localResult.computer = localResult.computer + 1;
     computerScore.textContent = `computer : ${localResult.computer}`;
     localStorage.setItem('result', JSON.stringify(localResult));
-  }
+ 
 }
 initialization.addEventListener('click', () => {
   localStorage.setItem('result', JSON.stringify({ computer: 0, user: 0 }));
   userScore.textContent = 'you : 0';
   computerScore.textContent = 'computer : 0';
-  resultMessage.textContent = '가위 바위 보!';
+  resultMessage.textContent = ' 가위 바위 보!';
 });
 restart.addEventListener('click', () => {
   isRotate = true;
   startInterval();
 });
+//----------------------------------------------------------
+const colorHTML = ['0', '3', '6', '9', 'C', 'F'];
 
-// function gameClosure() {
-//   function game() {
-//     /* This is just an example, replace this with the body of gameInit() */
-//     console.log('The game is running');
-//   }
+const randomRange = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-//   let itsInterval;
+class Particle {
+  constructor({ x, y, rotation, color, xSize, ySize, duration }) {
+    this.x = x;
+    this.y = y;
+    this.rotation = rotation;
+    this.color = color;
+    this.xSize = xSize;
+    this.ySize = ySize;
+    this.duration = duration;
+    this.children = document.createElement('div');
+    this.parent = resultMessage;
+  }
 
-//   return {
-//     start() {
-//       isRotate = !isRotate;
-//       itsInterval = setInterval(() => {
-//         let xx = 0.1;
-//         for (let el of test) {
-//           setTimeout(() => (icons.className = el), xx * 1000);
-//           xx += 0.1;
-//         }
-//       }, 100);
-//     },
-//     stop() {
-//       const userResult = document.querySelector('#userResult');
-//       const iconsResult = document.querySelector('#realResult');
-//       btns.addEventListener('click', (e) => {
-//         if (isRotate === false) {
-//           return;
-//         }
-//         if (test.includes(e.target.className) === false) {
-//           return;
-//         }
-//         isRotate = !isRotate;
-//         clearInterval(itsInterval);
-//         icons.style.display = 'none';
-//         iconsResult.className = test[Math.floor(Math.random() * test.length)];
-//         iconsResult.style.display = 'block';
-//         userResult.className = e.target.className;
+  set() {
+    this.children.style.setProperty('--x', this.x + 'px');
+    this.children.style.setProperty('--y', this.y + 'px');
+    this.children.style.setProperty('--rotate', this.rotation + 'deg');
+    this.children.style.setProperty('--color', '#' + this.color);
+    this.children.style.setProperty('--xSize', this.xSize + 'px');
+    this.children.style.setProperty('--ySize', this.ySize + 'px');
+    this.children.style.setProperty('--duration', this.duration + 'ms');
+    this.children.className = 'rect';
+    this.parent.append(this.children);
+  }
 
-//         if (e.target.className === iconsResult.className) {
-//           gameResult('tie');
-//         } else if (e.target.className === test[0]) {
-//           if (iconsResult.className === test[2]) {
-//             gameResult('lose');
-//           } else {
-//             gameResult('win');
-//           }
-//         } else if (e.target.className === test[1]) {
-//           if (iconsResult.className === test[0]) {
-//             gameResult('lose');
-//           } else {
-//             gameResult('win');
-//           }
-//         } else if (e.target.className === test[2]) {
-//           if (iconsResult.className === test[1]) {
-//             gameResult('lose');
-//           } else {
-//             gameResult('win');
-//           }
-//         }
-//       });
-//     },
-//   };
-// }
+  animate() {
+    this.set();
 
-// let game = gameClosure();
+    const timer = setTimeout(() => {
+      this.parent.removeChild(this.children);
+      clearTimeout(timer);
+    }, this.duration);
+  }
+}
 
-// btns.onclick = game.stop();
-// restart.onclick = game.start();
+function animateParticles({ total = 120 }) {
+  for (let i = 0; i < total; i++) {
+    const particle = new Particle({
+      x: randomRange(-300, 300),
+      y: randomRange(-300, 0),
+      rotation: randomRange(-360, 360),
+      color: `${colorHTML[randomRange(0, 6)]}${colorHTML[randomRange(0, 6)]}${
+        colorHTML[randomRange(0, 6)]
+      }`,
+      xSize: randomRange(4, 7),
+      ySize: randomRange(2, 10),
+      duration: randomRange(500, 1000),
+    });
+    particle.animate();
+  }
+}
+//---------------
