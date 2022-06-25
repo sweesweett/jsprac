e873dc39359b1df05fdd3bd6289f496a9bb65f23
sweesweett <sweesweet@naver.com>
2022-06-25 16:00:42 +0900

    뒤죽박죽 슬라이더

--- imgslider.js
+++ imgslider.js
@@ -3,16 +3,23 @@ console.log(imgWrapper.children);
 let num = 1;
 function plusMinus() {
   document.querySelector('#prevBtn').onclick = () => {
-    if (num === 0) {
+    if (num === 1) {
       num = imgWrapper.children.length;
     } else num--;
+    moveTo();
     console.log(num);
   };
   document.querySelector('#nextBtn').onclick = () => {
     if (num === imgWrapper.children.length) {
       num = 1;
     } else num++;
+    moveTo();
+
     console.log(num);
   };
 }
 plusMinus();
+function moveTo() {
+  document.querySelector('.active').classList.remove('active');
+  document.querySelector(`.img${num}`).classList.add('active');
+}
