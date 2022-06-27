b14e5ef43ee89a13dced9feb2aae51503e63f41d
sweesweett <sweesweet@naver.com>
2022-06-27 22:34:57 +0900

    기본 틀 완성

--- imgslider.js
+++ imgslider.js
@@ -20,6 +20,22 @@ function plusMinus() {
 }
 plusMinus();
 function moveTo() {
+  document.querySelector('.prev').classList.remove('prev');
+  document.querySelector('.next').classList.remove('next');
   document.querySelector('.active').classList.remove('active');
   document.querySelector(`.img${num}`).classList.add('active');
+  if (num === imgWrapper.children.length) {
+    imgWrapper.firstElementChild.classList.add('next');
+    document
+      .querySelector('.active')
+      .previousElementSibling.classList.add('prev');
+  } else if (num === 1) {
+    imgWrapper.lastElementChild.classList.add('prev');
+    document.querySelector('.active').nextElementSibling.classList.add('next');
+  } else {
+    document.querySelector('.active').nextElementSibling.classList.add('next');
+    document
+      .querySelector('.active')
+      .previousElementSibling.classList.add('prev');
+  }
 }
