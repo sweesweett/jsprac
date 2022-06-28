ec2c1c902474b40b5bdd559226234ab28e02f1c9
sweesweett <sweesweet@naver.com>
2022-06-28 10:39:55 +0900

    슬라이더 css 제외 만들음. 사소한 오류발생

--- imgslider.js
+++ imgslider.js
@@ -1,29 +1,29 @@
 const imgWrapper = document.querySelector('.img__wrapper');
 console.log(imgWrapper.children);
 let num = 1;
+plusMinus();
 function plusMinus() {
   document.querySelector('#prevBtn').onclick = () => {
     if (num === 1) {
       num = imgWrapper.children.length;
     } else num--;
     moveTo();
-    console.log(num);
   };
   document.querySelector('#nextBtn').onclick = () => {
     if (num === imgWrapper.children.length) {
       num = 1;
     } else num++;
     moveTo();
-
-    console.log(num);
   };
 }
-plusMinus();
+
 function moveTo() {
+  document.querySelector('.colored').classList.remove('colored');
   document.querySelector('.prev').classList.remove('prev');
   document.querySelector('.next').classList.remove('next');
   document.querySelector('.active').classList.remove('active');
   document.querySelector(`.img${num}`).classList.add('active');
+  document.querySelector(`.circle${num}`).classList.add('colored');
   if (num === imgWrapper.children.length) {
     imgWrapper.firstElementChild.classList.add('next');
     document
@@ -39,3 +39,13 @@ function moveTo() {
       .previousElementSibling.classList.add('prev');
   }
 }
+function navigateImg(e) {
+  if (e.target.classList.contains('circle')) {
+    const className = e.target.classList[1];
+    num = Number(className[className.length - 1]);
+    moveTo();
+  } else {
+    return;
+  }
+}
+document.querySelector('.sliderCircle').onclick = navigateImg;
