d93f2099bd240acf9e1afe1d5294c33fadce0fcb
sweesweett <sweesweet@naver.com>
2022-07-14 23:04:25 +0900

    카드js 함수만들기

--- card.js
+++ card.js
@@ -7,11 +7,28 @@ const imgs = [
 ];
 let cardCount = 0;
 for (let i = 0; i < imgs.length; i++) {
-  appendCard();
+  prependCard();
 }
-function appendCard() {
+let startX = 0, //마우스가 터치를 시작한 위치 x
+  startY = 0, // 마우스가 터치를 시작한 위치 y
+  moveX = 0,
+  moveY = 0;
+const firstCard = document.querySelector('.card:last-child');
+const secondCard = document.querySelector('.card:nth-last-child(2)');
+const thirdCard = document.querySelector('.card:nth-last-child(3)');
+addEventListener(firstCard);
+function prependCard() {
   const newCard = document.createElement('div');
   newCard.className = 'card';
   newCard.style.backgroundImage = `url(${imgs[cardCount++ % imgs.length]})`;
-  frame.append(newCard); //아 맨 뒤인 애가 앞에오는 이유가 html은 늦게 입력된애를 맨위로 올리지....
+  frame.prepend(newCard); //아 맨 뒤인 애가 앞에오는 이유가 html은 늦게 입력된애를 맨위로 올리지....
+}
+
+function addEventListener(card) {
+  card.addEventListener('pointerdown', onPointerDown);
+}
+function onPointerDown(e) {
+  startX = e.clientX;
+  startY = e.clientY;
+  console.log(startX, startY);
 }
