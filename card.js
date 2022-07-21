8545d5e48e3285aa85e08fdfba3bd31c25cb2137
sweesweett <sweesweet@naver.com>
2022-07-21 21:53:18 +0900

    카드스와이퍼

--- card.js
+++ card.js
@@ -14,8 +14,8 @@ let startX = 0, //마우스가 터치를 시작한 위치 x
   moveX = 0,
   moveY = 0;
 let firstCard = document.querySelector('.card:last-child');
-const secondCard = document.querySelector('.card:nth-last-child(2)');
-const thirdCard = document.querySelector('.card:nth-last-child(3)');
+const likeBg = document.querySelector('.likeBackground');
+const dislikeBg = document.querySelector('.dislikeBackground');
 addEventListener(firstCard);
 function prependCard() {
   const newCard = document.createElement('div');
@@ -27,17 +27,15 @@ function prependCard() {
 function addEventListener(card) {
   card.addEventListener('pointerdown', onPointerDown);
 }
-function setTransform(x, y, deg, dur) {
-  firstCard.style.transform = `translate3d(${x}px, ${y}px, 0) rotate(${deg}deg)`;
+function setTransform(thing, x, y, deg, dur) {
+  thing.style.transform = `translate3d(${x}px, ${y}px, 0) rotate(${deg}deg)`;
   if (dur) {
-    firstCard.style.transition = `transform ${dur}ms`;
+    thing.style.transition = `transform ${dur}ms`;
   }
 }
 function onPointerDown(e) {
   startX = e.clientX;
   startY = e.clientY;
-  console.log(startX, startY);
-  console.log(firstCard);
   firstCard.addEventListener('pointermove', onPointerMove);
   firstCard.addEventListener('pointerup', onPointerUp);
   firstCard.addEventListener('pointerleave', onPointerUp);
@@ -45,8 +43,14 @@ function onPointerDown(e) {
 function onPointerMove(e) {
   moveX = e.clientX - startX;
   moveY = e.clientY - startY;
-  setTransform(moveX, moveY, (moveX / innerWidth) * 60, 100);
-  console.log(moveX, moveY);
+  if (moveX < 0) {
+    likeBg.style.opacity = 1;
+    dislikeBg.style.opacity = 0;
+  } else if (moveX > 0) {
+    dislikeBg.style.opacity = 1;
+    likeBg.style.opacity = 0;
+  }
+  setTransform(firstCard, moveX, moveY, (moveX / innerWidth) * 60, 100);
 }
 function onPointerUp(e) {
   firstCard.removeEventListener('pointermove', onPointerMove);
@@ -57,18 +61,32 @@ function onPointerUp(e) {
     firstCard.removeEventListener('pointerdown', onPointerDown);
     fly();
   } else {
-    setTransform(0, 0, 0);
+    setTransform(firstCard, 0, 0, 0);
+    likeBg.style.opacity = 0;
+    dislikeBg.style.opacity = 0;
   }
 }
 function fly() {
   const flyX = (Math.abs(moveX) / moveX) * innerWidth * 1.1;
   const flyY = (moveY / moveX) * flyX;
-  setTransform(flyX, flyY, (flyX / innerWidth) * 80, innerWidth * 0.8);
+
+  setTimeout(() => (likeBg.style.opacity = 0), 500);
+  setTransform(
+    firstCard,
+    flyX,
+    flyY,
+    (flyX / innerWidth) * 80,
+    innerWidth * 0.8
+  );
+
   let prev = firstCard;
   firstCard = prev.previousElementSibling;
   setTimeout(() => frame.removeChild(prev), 500);
   prependCard(prev);
   addEventListener(firstCard);
+
+  likeBg.style.opacity = 0;
+  dislikeBg.style.opacity = 0;
 }
 document.getElementById('like').onclick = () => {
   moveX = -1;
