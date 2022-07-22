af7ff2a90300670bc1ceca24207c929a7716402e
sweesweett <sweesweet@naver.com>
2022-07-22 22:52:26 +0900

    터치슬라이더구현

--- imgslider2.js
+++ imgslider2.js
@@ -1,6 +1,9 @@
 const imgWrapper = document.querySelector('.img__wrapper');
+// const visibleImg = document.querySelector('.img__visible');
 const newWidth = document.querySelector('.img__visible').offsetWidth;
 let num = 0;
+let startX = 0,
+  moveX = 0;
 document.querySelector('#nextBtn').onclick = () => {
   if (num === imgWrapper.children.length - 1) {
     num = 0;
@@ -8,7 +11,7 @@ document.querySelector('#nextBtn').onclick = () => {
     num++;
   }
 
-  imgWrapper.style.left = `-${num * newWidth}px`;
+  imgWrapper.style.transform = `translateX(-${num * newWidth}px)`;
 };
 document.querySelector('#prevBtn').onclick = () => {
   if (num === 0) {
@@ -17,5 +20,18 @@ document.querySelector('#prevBtn').onclick = () => {
     num--;
   }
 
-  imgWrapper.style.left = `-${num * newWidth}px`;
+  imgWrapper.style.transform = `translateX(-${num * newWidth}px)`;
 };
+// visibleImg.addEventListener('pointerdown', onPointerDown);
+// function onPointerDown(e) {
+//   startX = e.clientX;
+
+//   console.log(startX);
+//   visibleImg.addEventListener('pointermove', onPointerMove);
+// }
+// function onPointerMove(e) {
+//   moveX = e.clientX - startX;
+
+//   console.log(moveX);
+//   imgWrapper.style.transform = `rotate(${moveX}deg)`;
+// } // 왜안될까
